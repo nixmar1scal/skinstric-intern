26a448d29cfdd9e34d99b3f7e71eb83326b87828
Nicolas <nicolas8mk@gmail.com>
2025-07-25 12:31:25 -0500

    final touch

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -41,7 +41,7 @@ const Intro = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.6 }}
+      transition={{ duration: 0.5 }}
     >
       <Header />
       <div className="to-start">
